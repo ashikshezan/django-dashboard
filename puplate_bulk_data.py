23034fda2dbf5f83179106825c261cb9fd74ab85
ashikshezan <ashikshezan@gmail.com>
2020-05-30 19:21:49 +0600

    database + pandas

--- puplate_bulk_data.py
+++ puplate_bulk_data.py
@@ -1,12 +1,32 @@
 import pandas as pd
 from user_dashboard.models import Dashboard
-from user_dashboard.views import format_and_clean_data
+
+
+def format_and_clean_data(data):
+    # Renaming Columns
+    data.columns = [
+        'idd'
+        'directorate',
+        'department',
+        'expenditure_type',
+        'ref_doc_1',
+        'vendor_name',
+        'expenditure_amount',
+        'payment_date']
+
+    # Converting payment date to pandas -> Datetime object & Creating 3 new Columns
+    data['payment_date'] = pd.to_datetime(data['payment_date'])
+    data['year'] = pd.DatetimeIndex(data['payment_date']).year
+    data['month'] = pd.DatetimeIndex(data['payment_date']).month
+    # Creating a new column with full month name
+    data['month'] = data['payment_date'].map(lambda x: x.strftime('%B'))
+
+    return data
 
 
 df = pd.read_csv('data.csv')
 df = format_and_clean_data(df)
 print(df.head(3))
-
 row_iter = df.iterrows()
 
 objs = [
--- user_dashboard/views.py
+++ user_dashboard/views.py
@@ -11,38 +11,26 @@ from . import dashboard_plot_items
 # Dashboard Home View
 @login_required
 def dashboar_view(request, *args, **kwargs):
-    context = dict()
-    # # temporarily getting the data from the data.csv file
-    # df = pd.read_csv('data.csv')
-    # df = format_and_clean_data(data=df)
-
     # Loading Data from Django Model
     dashboard_items = Dashboard.objects.all()
     df = read_frame(dashboard_items)
+    df = format_and_clean_data(df)
     print(df.head(3))
-    # # Creatings Charts
-    # context['pie_plot'] = dashboard_plot_items.plot_pei_chart(df)  # pie chart
-    # context['pie_plot_dropdown'] = dashboard_plot_items.dropdown_pie(
-    #     dataframe=df)  # dropdown pie chart
-    # context['bar_chart_yearly'] = dashboard_plot_items.yearly_expenditure_barchart(
-    #     dataframe=df)  # yearly expenditure bar chart
-    # context['time_series'] = dashboard_plot_items.time_series(
-    #     dataframe=df)  # expenditure timeseries
+
+    context = dict()
+    # Creatings Charts
+    context['pie_plot'] = dashboard_plot_items.plot_pei_chart(df)  # pie chart
+    context['pie_plot_dropdown'] = dashboard_plot_items.dropdown_pie(
+        dataframe=df)  # dropdown pie chart
+    context['bar_chart_yearly'] = dashboard_plot_items.yearly_expenditure_barchart(
+        dataframe=df)  # yearly expenditure bar chart
+    context['time_series'] = dashboard_plot_items.time_series(
+        dataframe=df)  # expenditure timeseries
 
     return render(request, template_name='pages/dashboard.html', context=context)
 
 
 def format_and_clean_data(data):
-    # Renaming Columns
-    data.columns = [
-        'directorate',
-        'department',
-        'expenditure_type',
-        'ref_doc_1',
-        'vendor_name',
-        'expenditure_amount',
-        'payment_date']
-
     # Converting payment date to pandas -> Datetime object & Creating 3 new Columns
     data['payment_date'] = pd.to_datetime(data['payment_date'])
     data['year'] = pd.DatetimeIndex(data['payment_date']).year
